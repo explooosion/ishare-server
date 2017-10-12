cb9b2ea3a715985c73575cfe2c49ffa4ed0b85e7
Robby <ta7382@gmail.com>
2017-10-12 21:59:24 +0800

    修正 - 語法錯誤(bug)

--- src/models/user.js
+++ src/models/user.js
@@ -6,45 +6,46 @@ import config from '../config/db';
 class User {
     async login(ctx) {
         let group = ctx.request.body.loginbar;
+        console.log(ctx.request.body);
         switch (group) {
-            case '1'://社福單位
+            case '1': //社福單位
                 try {
                     const connection = await mysql.createConnection(config);
                     const [rows, fields] = await connection.query(
-                        'select * from web_service where serviceusername = ? and servicepassword = ?', [ctx.request.body.userId, tx.request.body.userPwd]
+                        'select * from web_service where serviceusername = ? and servicepassword = ?', [ctx.request.body.userId, ctx.request.body.userPwd]
                     );
                     return rows;
                 } catch (e) {
                     return false;
                 }
                 break;
-            case '2'://店家
+            case '2': //店家
                 try {
                     const connection = await mysql.createConnection(config);
                     const [rows, fields] = await connection.query(
-                        'select * from web_store where storeusername = ? and storepassword = ?', [ctx.request.body.userId, tx.request.body.userPwd]
+                        'select * from web_store where storeusername = ? and storepassword = ?', [ctx.request.body.userId, ctx.request.body.userPwd]
                     );
                     return rows;
                 } catch (e) {
                     return false;
                 }
                 break;
-            case '3'://老師
+            case '3': //老師
                 try {
                     const connection = await mysql.createConnection(config);
                     const [rows, fields] = await connection.query(
-                        'select * from web_teacher where teacherusername = ? and teacherpassword = ?', [ctx.request.body.userId, tx.request.body.userPwd]
+                        'select * from web_teacher where teacherusername = ? and teacherpassword = ?', [ctx.request.body.userId, ctx.request.body.userPwd]
                     );
                     return rows;
                 } catch (e) {
                     return false;
                 }
                 break;
-            case '4'://學生
+            case '4': //學生
                 try {
                     const connection = await mysql.createConnection(config);
                     const [rows, fields] = await connection.query(
-                        'select * from web_student where childusername = ? and childpassword = ?', [ctx.request.body.userId, tx.request.body.userPwd]
+                        'select * from web_student where childusername = ? and childpassword = ?', [ctx.request.body.userId, ctx.request.body.userPwd]
                     );
                     return rows;
                 } catch (e) {
