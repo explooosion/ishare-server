8a1540a449277d406cd0394903d438078fc57714
Robby <ta7382@gmail.com>
2017-10-12 22:08:03 +0800

    修正 - 群組分類用數字表示

--- src/models/user.js
+++ src/models/user.js
@@ -8,40 +8,43 @@ class User {
         let group = ctx.request.body.loginbar;
         console.log(ctx.request.body);
         switch (group) {
-            case '1': //社福單位
+            case 1: //社福單位
                 try {
                     const connection = await mysql.createConnection(config);
                     const [rows, fields] = await connection.query(
                         'select * from web_service where serviceusername = ? and servicepassword = ?', [ctx.request.body.userId, ctx.request.body.userPwd]
                     );
+                    console.log(rows);
                     return rows;
                 } catch (e) {
                     return false;
                 }
                 break;
-            case '2': //店家
+            case 2: //店家
                 try {
                     const connection = await mysql.createConnection(config);
                     const [rows, fields] = await connection.query(
                         'select * from web_store where storeusername = ? and storepassword = ?', [ctx.request.body.userId, ctx.request.body.userPwd]
                     );
+                    console.log(rows);
                     return rows;
                 } catch (e) {
                     return false;
                 }
                 break;
-            case '3': //老師
+            case 3: //老師
                 try {
                     const connection = await mysql.createConnection(config);
                     const [rows, fields] = await connection.query(
                         'select * from web_teacher where teacherusername = ? and teacherpassword = ?', [ctx.request.body.userId, ctx.request.body.userPwd]
                     );
+                    console.log(rows);
                     return rows;
                 } catch (e) {
                     return false;
                 }
                 break;
-            case '4': //學生
+            case 4: //學生
                 try {
                     const connection = await mysql.createConnection(config);
                     const [rows, fields] = await connection.query(
